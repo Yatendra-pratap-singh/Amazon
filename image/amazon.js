658eeff5cc7e8f818f0f7b79f2cdb3ce746875b7
Yatendra-pratap-singh <yatendra533@gmail.com>
2023-10-13 11:17:20 +0530

    initial file

---
+++ image/amazon.js
@@ -0,0 +1,9 @@
+const openSidebar = () => {
+    document.getElementById("side_bar1").style.width = "340px";
+    // document.getElementById("").style.marginLeft = "0";
+};
+
+const closeSidebar = () => {
+    document.getElementById("side_bar1").style.width = "0";
+    // document.getElementById("menu").style.marginLeft = "0";
+};
