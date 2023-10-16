cb9644e0d410151a3efb2a0b620327221bd1e126
Yatendra-pratap-singh <yatendra533@gmail.com>
2023-10-16 10:57:18 +0530

    correct js file

--- amazon.js
+++ amazon.js
@@ -1,5 +1,5 @@
 function openSidebar(){
-document.getElementById("side_bar1").style.width = "350px";
+document.getElementById("side_bar1").style.width = "335px";
 }
 function closeSidebar(){
     document.getElementById("side_bar1").style.width = "0px";
